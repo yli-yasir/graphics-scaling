17cc1cdfa1379b371a6bd0f47d9addc3cb44a074
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-08-19 20:16:53 +0300

    explain, logical and display pixels

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 
 window.onresize = () => {
   const screenHeight = window.innerHeight;
   const screenWidth = window.innerWidth;
 
-  // Resize while maintaining aspect ratio
-
   // The pixels the canvas consists of are called logical pixels.
-  // The number of logical pixels attributes are defined on the canvas elements 
+  // The number of logical pixels attributes are defined on the canvas elements
   // via the height and width attributes.
 
+  // The number of pixels that are rendered on the screen to display the canvas
+  // are called display pixels.
+  // They can be set via the style.width and style.height properties of the canvas.
 
+  // We can should the display size of the canvas to scale it while leaving
+  // the logical size of the canvas untouched.
 
+  
 };
