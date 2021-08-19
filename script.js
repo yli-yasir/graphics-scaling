ff92966fc8bfa9f12e4b9788b5083dbb707081e9
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-08-19 20:10:12 +0300

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas');
+
+window.onresize = () => {
+  const screenHeight = window.innerHeight;
+  const screenWidth = window.innerWidth;
+
+  // Resize while maintaining aspect ratio
+
+  // The pixels the canvas consists of are called logical pixels.
+  // The number of logical pixels attributes are defined on the canvas elements 
+  // via the height and width attributes.
+
+
+
+};
