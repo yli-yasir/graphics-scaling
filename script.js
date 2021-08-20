31512d1f26391f8a4e07ce35778fb0ed87a974a0
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-08-20 09:49:30 +0300

    add scale func

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const canvas = document.querySelector("canvas");
 
-window.onresize = () => {
+// The pixels the canvas consists of are called logical pixels.
+// The logical size of the canvas is defined on the canvas HTML element
+// via the height and width attributes.
+
+// The number of pixels that are rendered on the screen to display the canvas
+// are called display pixels.
+// They can be set via the style.width and style.height properties of the canvas.
+
+// We can should resize the display size of the canvas while leaving the logical size untouched to scale it.
+
+// Let's define the following methods for convenience.
+canvas.setDisplayWidth = function (width) {
+  this.style.width = width + "px";
+};
+
+canvas.setDisplayHeight = function (height) {
+  this.style.height = height + "px";
+};
+
+function scale() {
   // Get the canvas aspect ratio...
   // Alternatively, we can also say that we are getting the size of the width in respect to the height.
 
@@ -29,8 +48,8 @@ window.onresize = () => {
     // We conclude that we can safely accomodate the canvas, without any overflow, by scaling its display height to screens height
     // then calculating the canvas's display width by multiplying it by its aspect ratio in order to maintain the ratio.
 
-    canvas.style.height = screenHeight;
-    canvas.style.width = canvas.style.height * canvasAspectRatio;
+    canvas.setDisplayHeight(screenHeight);
+    canvas.setDisplayWidth(screenHeight * canvasAspectRatio);
 
     // The display width of the canvas will never overflow because the canvas display width will never be larger than the
     // the screen width when it's calculated based on the height that its just been scaled to.
@@ -63,7 +82,13 @@ window.onresize = () => {
     // Since screen width is divided by a smaller value, screen height  will always be larger than canvas Height
     // screenHeight > canvasDisplayHeight
     // e.g. screenWidth / 1.6 > screenWidth / 1.7
-    canvas.style.width = screenWidth;
-    canvas.style.height = screenWidth / canvasAspectRatio;
+    canvas.setDisplayWidth(screenWidth);
+    canvas.setDisplayHeight(screenWidth / canvasAspectRatio);
   }
-};
+}
+
+// Scale once at the start
+scale();
+
+// Also whenever the screen is resized...
+window.onresize = scale;
